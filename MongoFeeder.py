fb28f7bc971646cc3d1387417414f42044f7421c
kayne10 <tkayne10@gmail.com>
2017-04-23 13:04:49 -0600

    edited index view for frontend and used geojson for map api

--- MongoFeeder.py
+++ MongoFeeder.py
@@ -1,23 +1,32 @@
+import os
 import json
 from pymongo import MongoClient
 #connect to database
 c = MongoClient('localhost',27017)
 db = c.ddt
 
+# change nodeInfo to db.tweets to insert Tweets
 nodeInfo = db.tweets
+# nodeInfo = db.coordinates
 table = db.table
 
-# Parse JSON
-with open('shineynewset/20170419205436_#hiring.json', 'rb') as f:
-    for row in f:
-        # insert into Mongo
-        nodeInfo.insert_one(json.loads(row))
-
+if os.path.exists('shineynewset'):
+    # monitor this folder
 
 # JSON FILES
-# 20170419205433_#hiring.json
-# 20170419205436_#hiring.json
-# 20170419205439_#hiring.json
-# 20170419205441_#hiring.json
-# 20170419205444_#hiring.json
-# 20170419205446_#hiring.json
+datasets = [
+    '20170419205433_#hiring.json',
+    '20170419205436_#hiring.json',
+    '20170419205439_#hiring.json',
+    '20170419205441_#hiring.json',
+    '20170419205444_#hiring.json',
+    '20170419205446_#hiring.json',
+]
+
+# Parse JSON
+
+for dataset in datasets:
+    with open('shineynewset/' + dataset, 'rb') as f:
+        for row in f:
+            # insert into Mongo
+            nodeInfo.insert_one(json.loads(row))
