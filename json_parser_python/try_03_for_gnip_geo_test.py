5fa05d129ca71916068b10df4652073f197cc417
amirkashi <amka6009@colorado.edu>
2017-04-23 15:19:53 -0600

    pythond code for prosessing bach ot json file version 1 is added

---
+++ json_parser_python/try_03_for_gnip_geo_test.py
@@ -0,0 +1,76 @@
+import json
+import unicodedata
+
+
+json_data = open('file_01.json').readlines()
+json_format = open('sample.json', 'w')
+json_geo_format_google_map = open('google_map.json', 'w')
+
+json_out ={}
+features = []
+
+Type = {}
+Type["type"] = "FeatureCollection"
+json_out.update(Type)
+
+for lines in json_data:
+	data = json.loads(lines)
+	marker = {} # all feature of a marker saves here 
+	tp ={}
+	tp["type"] = "Feature"
+	marker.update(tp)
+	# -----------------------------------------------
+	# property dictionary 
+	prop = {}
+	name = {}
+	name["Name"] = data['actor']['preferredUsername'].encode('utf8')
+	date = {}
+	date["Date"] = data['object']['postedTime'][0:10].encode('utf8')
+	body = {}
+	body["body"] = data['body'].encode('utf8')
+	prop["properties"] = name
+	prop["properties"].update(date)
+	prop["properties"].update(body)
+	marker.update(prop)
+	#------------------------------------------------
+	# geometry dictionary 
+	geo = {}
+	tp2 = {}
+	tp2["type"] = "Point"
+	long_lat = []
+	coord = {}
+	if 'profileLocations' in data['gnip']:
+		lng = 0.0
+		lat = 0.0
+		for items in data['gnip']['profileLocations']:
+			lng = float( items['geo']['coordinates'][0])
+			lat = float (items['geo']['coordinates'][1])
+			if lng > 0:
+				lng = float( items['geo']['coordinates'][1])
+				lat = float (items['geo']['coordinates'][0])
+		long_lat.append(lng)
+		long_lat.append(lat)	
+	else:
+		long_lat.append(0)
+		long_lat.append(0)
+	
+	coord["coordinates"]= long_lat
+	geo["geometry"] = tp2
+	geo["geometry"].update(coord)
+	
+	
+	#marker.update(prop)
+	marker.update(geo)
+	if long_lat[0] != 0:
+		features.append(marker)
+
+json_out["features"] = features
+#print json_out
+js = json.dumps(json_out, indent=4)
+print >> json_format, js
+print >> json_geo_format_google_map, "eqfeed_callback("
+print >> json_geo_format_google_map, js
+print >> json_geo_format_google_map, ")"
+json_format.close()
+json_geo_format_google_map.close()
+#print json_out 
---
+++ json_parser_python/try_04_for_gnip_geo_batch.py
@@ -0,0 +1,84 @@
+import json
+import unicodedata
+import os
+
+
+def json_output(input_file):
+	json_out ={}
+	features = []
+
+	Type = {}
+	Type["type"] = "FeatureCollection"
+	json_out.update(Type)
+
+	for lines in input_file:
+		data = json.loads(lines)
+		marker = {} # all feature of a marker saves here 
+		tp ={}
+		tp["type"] = "Feature"
+		marker.update(tp)
+		# -----------------------------------------------
+		# property dictionary 
+		prop = {}
+		name = {}
+		name["Name"] = data['actor']['preferredUsername'].encode('utf8')
+		date = {}
+		date["Date"] = data['object']['postedTime'][0:10].encode('utf8')
+		body = {}
+		body["body"] = data['body'].encode('utf8')
+		prop["properties"] = name
+		prop["properties"].update(date)
+		prop["properties"].update(body)
+		marker.update(prop)
+		#------------------------------------------------
+		# geometry dictionary 
+		geo = {}
+		tp2 = {}
+		tp2["type"] = "Point"
+		long_lat = []
+		coord = {}
+		if 'profileLocations' in data['gnip']:
+			lng = 0.0
+			lat = 0.0
+			for items in data['gnip']['profileLocations']:
+				lng = float( items['geo']['coordinates'][0])
+				lat = float (items['geo']['coordinates'][1])
+				#if lng > 0:
+				#	lng = float( items['geo']['coordinates'][1])
+				#	lat = float (items['geo']['coordinates'][0])
+			long_lat.append(lng)
+			long_lat.append(lat)	
+		else:
+			long_lat.append(0)
+			long_lat.append(0)
+		
+		coord["coordinates"]= long_lat
+		geo["geometry"] = tp2
+		geo["geometry"].update(coord)
+		
+		
+		#marker.update(prop)
+		marker.update(geo)
+		if long_lat[0] != 0:
+			features.append(marker)
+
+	json_out["features"] = features
+	return json_out
+
+
+def main():
+	path_read 	= 	'/home/user/Dropbox/CU/ATLS_5214/project/DataDreamTeam/shineynewset/'
+	path_write 	= 	'/home/user/Dropbox/CU/ATLS_5214/project/DataDreamTeam/geo_jsons_twits/'
+	for filename in os.listdir(path_read):
+		if filename.endswith('.json'): 
+			read_file = open(path_read+'/'+filename, 'r').readlines()
+			write_file = open(path_write+'/'+filename,'w')
+			geo_json = json_output(read_file)
+			
+			print >> write_file, geo_json
+			#read_file.close()
+			write_file.close()
+			
+main()
+
+
