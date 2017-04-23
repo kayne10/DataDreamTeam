db459765d8836904c3b7bc8b2a143226ce3fc2c5
kayne10 <tkayne10@gmail.com>
2017-04-22 18:29:14 -0600

    Attempted to parse all tweets to form a new Mongo collection of GeoJSON data

---
+++ Website/models/coordinates.js
@@ -0,0 +1,20 @@
+// var mongoose = require("mongoose");
+// var Schema = mongoose.Schema;
+// var ObjectId = Schema.Types.ObjectId;
+//
+//
+// var coordinateSchema = new Schema({
+//   type: {type:String, required: true},
+//   coordinates: [Number]
+//   // body: {type: String, required: true}
+// });
+//
+// module.exports = mongoose.model('Coordinates', coordinateSchema);
+
+var GeoJSON = require('mongoose-geojson-schema');
+var mongoose = require('mongoose');
+
+var coordinateSchema = mongoose.Schema({
+  feature: mongoose.Schema.Types.Feature,
+  geometry: mongoose.Schema.Types.Geometry
+});
