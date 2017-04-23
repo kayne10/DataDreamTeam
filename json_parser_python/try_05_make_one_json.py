d36562144eee530abec9e09cc80e54f514cc8e4d
amirkashi <amka6009@colorado.edu>
2017-04-23 17:58:43 -0600

    python code for making one geo json from all twits added

--- json_parser_python/try_05_make_one_json.py
+++ json_parser_python/try_05_make_one_json.py
@@ -4,12 +4,8 @@ import os
 
 
 def json_output(input_file):
-	json_out ={}
-	features = []
 
-	Type = {}
-	Type["type"] = "FeatureCollection"
-	json_out.update(Type)
+	features = []
 
 	for lines in input_file:
 		data = json.loads(lines)
@@ -43,9 +39,7 @@ def json_output(input_file):
 			for items in data['gnip']['profileLocations']:
 				lng = float( items['geo']['coordinates'][0])
 				lat = float (items['geo']['coordinates'][1])
-				#if lng > 0:
-				#	lng = float( items['geo']['coordinates'][1])
-				#	lat = float (items['geo']['coordinates'][0])
+
 			long_lat.append(lng)
 			long_lat.append(lat)	
 		else:
@@ -61,25 +55,35 @@ def json_output(input_file):
 		marker.update(geo)
 		if long_lat[0] != 0:
 			features.append(marker)
-
-	json_out["features"] = features
-	return json_out
+	return features
+	#json_out["features"] = features
+	#return json_out
 
 
 def main():
+	json_out ={}
+	features = []
+
+	Type = {}
+	Type["type"] = "FeatureCollection"
+	json_out.update(Type)
+	
 	path_read 	= 	'/home/user/Dropbox/CU/ATLS_5214/project/DataDreamTeam/shineynewset/'
-	path_write 	= 	'/home/user/Dropbox/CU/ATLS_5214/project/DataDreamTeam/geo_jsons_twits/'
+	
+	one_json = open('one_json.json', 'w')
 	for filename in os.listdir(path_read):
 		if filename.endswith('.json'): 
 			read_file = open(path_read+'/'+filename, 'r').readlines()
-			write_file = open(path_write+'/'+filename,'w')
-			geo_json = json_output(read_file)
-			geo_json_dump = json.dumps(geo_json, indent=4)
-			#print >> write_file, "eqfeed_callback("
-			print >> write_file, geo_json_dump
-			#print >> write_file, ")"
-			write_file.close()
-			
+			twit_list = json_output(read_file) # return a list 
+			features = features + twit_list
+			#os.remove(path_read+'/'+filename)
+	json_out["features"] = features
+	geo_json_dump = json.dumps(json_out, indent=4)
+	print >> one_json, "eqfeed_callback("
+	print >> one_json, geo_json_dump
+	print >> one_json, ")"
+	one_json.close()
+	
 main()
 
 
